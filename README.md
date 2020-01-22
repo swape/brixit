d6e154558f48c2a0dc4337800afb804d40c21c53
Alireza Balouch <alireza.balouch@entur.org>
2020-01-22 21:43:44 +0100

    adding gitHub link

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # brixit
 
 Demo project to demonstrate parcel, lit-html and stylus.
+
+<https://github.com/swape/brixit>
+
+<https://brixit.swape.net>
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const myTemplate = () => html`
   <div class="container">
     Made by Alireza Balouch with lit-html, parcel and stylus.
   </div>
+  <div class="container"><a href="https://github.com/swape/brixit" target="_blank" rel="noopener">github.com/swape/brixit</a></div> 
 `
 
 const body = document.body
