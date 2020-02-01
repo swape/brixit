24a8405a0a78655f2487a86ed59c6d3aca1fc9df
Alireza Balouch <alireza.balouch@entur.org>
2020-02-01 17:51:08 +0100

    moving the footer to it's own template

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const header = html`
   <h1>BRIXIT</h1>
 `
 
+const footer = html`  <div class="container">Made by Alireza Balouch with lit-html, parcel and stylus.</div>
+  <div class="container"><a href="https://github.com/swape/brixit" target="_blank" rel="noopener">github.com/swape/brixit</a></div> `
+
 const myTemplate = () => html`
   ${header}
   <div class="container">${drawMatrix()}${drawControls()}</div>
-  <div class="container">Made by Alireza Balouch with lit-html, parcel and stylus.</div>
-  <div class="container"><a href="https://github.com/swape/brixit" target="_blank" rel="noopener">github.com/swape/brixit</a></div> 
+  ${footer}
 `
 
 const body = document.body
